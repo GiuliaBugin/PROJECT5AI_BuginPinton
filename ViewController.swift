d560970004acce9c841e9ad259ec9291a22db5a7
pintonleonardo <43903112+pintonleonardo@users.noreply.github.com>
2018-11-15 09:16:07 +0100

    fromStringToInt

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PROJECT5AI_buginPinton
+//
+//  Created by Giulia Bugin on 15/11/2018.
+//  Copyright © 2018 Giulia Bugin. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+func fromStirngToInt(var stringa) -> int
+{
+    let intero:Int! = Int(ciao)
+    return intero
+}
