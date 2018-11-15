5759bdf92da10102c08d755474124532d7e40f7a
Giulia <giuliabugin9@gmail.com>
2018-11-15 09:02:21 +0100

    code

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var area: UILabel!
+    @IBOutlet weak var lato: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func controlla(_ sender: Any) {
+        area.text = String(calcolaAreaQuadrato(fromStringToInt(lato.text)));
     }
 
 
